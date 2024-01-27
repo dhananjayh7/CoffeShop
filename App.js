bed1e19079c42a66dd00f808392ac2b2986e8390
dhananjayh7 <dhananjayh8@gmail.com>
2024-01-27 16:03:42 +0530

    new changes

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import CustomIcons from './src/components/CustomIcons'
 
 const App = () => {
   return (
     <View>
       <Text>Apps</Text>
+      <CustomIcons name="search" size={26} />
     </View>
   )
 }
